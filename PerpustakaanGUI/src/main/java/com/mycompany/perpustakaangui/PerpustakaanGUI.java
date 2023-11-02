527499f11b53bcbc2886d6dd2dd276ab4708da06
Septiancesare <112379597+Septiancesare@users.noreply.github.com>
2023-11-02 14:22:52 +0700

    tambah file

---
+++ PerpustakaanGUI/src/main/java/com/mycompany/perpustakaangui/PerpustakaanGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.perpustakaangui;
+
+/**
+ *
+ * @author Septiancesare
+ */
+public class PerpustakaanGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
